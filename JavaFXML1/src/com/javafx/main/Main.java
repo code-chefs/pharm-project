5e28b1eddfd5fa3c923d06350c6d5be66e356be6
chrisrepo <christopher.repanich@student.csulb.edu>
2015-04-09 12:22:23 -0700

    Connection Commit
    JDBC added, with lib mysql connector

--- JavaFXML1/src/com/javafx/main/Main.java
+++ JavaFXML1/src/com/javafx/main/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
       stage.setTitle("User Login");
       stage.setScene(scene);
       stage.show();
+      JDBC db = new JDBC();
    }
    
    public void loadPatientSearch() throws Exception {
